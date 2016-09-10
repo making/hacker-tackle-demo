cffa12b3eafb74a65a094ea0773f6d34bcebd822
making <makingx@gmail.com>
2016-09-10 14:00:30 +0900

    Hello World

---
+++ hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
